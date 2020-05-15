8d6fa5627fb8d13c91d5fa24e63d5469260732c8
Bendik Knapstad <knaben@OBOS.NO>
2020-05-15 13:20:37 +0200

    add load save

--- bot.py
+++ bot.py
@@ -1,6 +1,7 @@
 import tweepy
 import config
 import math
+import json
 
 auth = tweepy.OAuthHandler(config.API_key, config.API_secret_key)
 auth.set_access_token(config.access_token, config.access_token_secret)
@@ -17,6 +18,11 @@ def load_tekst():
 def load_index():
     with open("index.json") as file:
         return json.load(file)
+
+
+def save_index(index):
+    with open("index.json", "w") as file:
+        json.dump(index, file)
 
 
 def post_tweet(message):
@@ -39,3 +45,24 @@ def devide_post(message):
         return message_chunks
     else:
         return [message]
+
+
+def main():
+    tekst = load_tekst()
+    index = load_index()
+
+    try:
+        message = tekst[index]
+        while message.isupper():
+            index += 1
+            message += tekst[index]
+        for i in devide_post(message):
+            post_tweet(i)
+        index += 1
+        save_index(index)
+    except Exception as e:
+        print(e)
+
+
+if __name__ == "__main__":
+    main()
